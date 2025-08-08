7fc4232c02197264d7fcdcac8c0e1b57813f82f2
jamunesh15 <jsheta15@gmail.com>
2025-08-08 16:08:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(cors({
 
 // Enhanced preflight handling
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', 'https://ed-tech-platform-f-blush.vercel.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin, Cache-Control, X-File-Name');
